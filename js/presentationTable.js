8a2d267aac11e233d9704bc1b9493e26bdba0a5b
JackWagster <jack.wagster@gmail.com>
2016-03-14 11:36:44 -0700

    Wesbiste Testing

--- js/presentationTable.js
+++ js/presentationTable.js
@@ -10,10 +10,7 @@ function init_table(options) {
   $("#" + el).html("<table class='table table-striped table-condensed' id='my-table'></table>");
 
   $.when($.get(csv_path)).then(
-    function(data){
-
-      
-      
+    function(data){      
       var csv_data = $.csv.toArrays(data, csv_options);
 
       var table_head = "<thead><tr>";
@@ -29,17 +26,21 @@ function init_table(options) {
       for (row_id = 1; row_id < csv_data.length; row_id++) { 
         var row_html = "<tr>";
 
-          for (col_id = 0; col_id < csv_data[row_id].length; col_id++) { 
-            row_html += "<td>" + csv_data[row_id][col_id] + "</td>";
-          }
+			for (col_id = 0; col_id < csv_data[row_id].length; col_id++) { 
+				if(col_id != (csv_data[row_id].length-1))
+					row_html += "<td>" + csv_data[row_id][col_id] + "</td>";
+				else
+					row_html += "<td>" + "<button onclick='showAbs("+row_id+")'>" + "Click to View Abstract" + "</button>" + "<div hidden><p id= '" + row_id + "' hidden>" + csv_data[row_id][col_id] + "</p></div>"; + "</td>";
+					
+			}
           
         row_html += "</tr>";
         $('#my-table tbody').append(row_html);
       }
-
       $("#my-table").DataTable(datatables_options);
 
       if (allow_download)
         $("#" + el).append("<p><a class='btn btn-info' href='" + csv_path + "'><i class='glyphicon glyphicon-download'></i> Download as CSV</a></p>");
     });
+	
 }
